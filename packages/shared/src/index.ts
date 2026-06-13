8cc2c42d2e38631d97f343bf14bfadf82f4db313
Your Name <bhanupss137@gmail.com>
2026-06-13 23:55:54 +0530

    init app

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,11 @@
+export enum UserRole {
+  USER = "USER",
+  COMPANY = "COMPANY",
+  ADMIN = "ADMIN"
+}
+
+export enum SessionStatus {
+  SCHEDULED = "SCHEDULED",
+  IN_PROGRESS = "IN_PROGRESS",
+  COMPLETED = "COMPLETED"
+}
